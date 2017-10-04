53e05843284eb06f814d5e224c0607b0b855911d
SebastianG <sebastian.garren@gmail.com>
2017-10-04 15:03:09 -0500

    The sign-up meets all requirements but 1)doesn't show multiple errors atonce and 2)doesn't preserve what the user typed originally.

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
-
     
 
 @app.route('/welcome', methods=['POST'])
@@ -12,24 +10,37 @@ def welcome():
     password = request.form['password']
     verify_password = request.form['verify_password']
     name = request.form['user_name']
+    email = request.form['email']
 
     if len(name) < 3:
-        error = 'Name must be at least three characters.'
-        return redirect("/?error=" + error)
+        name_error = 'Name must be at least three characters.'
+        return redirect("/?name_error=" + name_error)
 
     if len(password) < 3 or len(password) > 20:
-        error = '''Password shouldn't be *that* dumb.'''
-        return redirect("/?error=" + error)
+        pass_error = '''Password shouldn't be *that* short.'''
+        return redirect("/?pass_error=" + pass_error)
 
     if password != verify_password:
-        error = '''Passwords don't match'''
-        return redirect("/?error=" + error)
-    else: 
+        veri_error = '''Passwords don't match'''
+        return redirect("/?veri_error=" + veri_error)
+
+    if len(email) > 1:
+        if '@' or '.' not in email:
+            if '' in email:
+                email_error = '''You need to have a proper email, mate.'''
+                return redirect("/?email_error=" + email_error)
+    else:
         return render_template('welcome.html', user_name=name)
 
 @app.route('/')
 def index():
-    form_error  = request.args.get('error')
-    return render_template('user-signup.html', error=form_error and cgi.escape(form_error, quote=True))
-
+    form_error1  = request.args.get('name_error', default='')
+    form_error2  = request.args.get('pass_error', default='')
+    form_error3  = request.args.get('veri_error', default='')
+    form_error4  = request.args.get('email_error', default='')
+    return render_template('user-signup.html', 
+    name_error=form_error1 and cgi.escape(form_error1, quote=True),
+    pass_error=form_error2 and cgi.escape(form_error2, quote=True),
+    veri_error=form_error3 and cgi.escape(form_error3, quote=True),
+    email_error=form_error4 and cgi.escape(form_error4, quote=True))
 app.run()
