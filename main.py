eed428a6d2b5571b836d9027a9d8ce8db0f58412
SebastianG <sebastian.garren@gmail.com>
2017-10-11 16:14:46 -0500

    Individual Error Messages

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-    
+
 
 @app.route('/welcome', methods=['POST'])
 def welcome():
@@ -12,35 +12,67 @@ def welcome():
     name = request.form['user_name']
     email = request.form['email']
 
-    if len(name) < 3:
-        name_error = 'Name must be at least three characters.'
-        return redirect("/?name_error=" + name_error)
+    password = cgi.escape(password)
+    verify_password = cgi.escape(verify_password)
+    name = cgi.escape(name)
+    email = cgi.escape(email)
+    is_error = 'False'
 
-    if len(password) < 3 or len(password) > 20:
-        pass_error = '''Password shouldn't be *that* short.'''
-        return redirect("/?pass_error=" + pass_error)
+    return render_template('welcome.html', user_name=name)
 
-    if password != verify_password:
-        veri_error = '''Passwords don't match'''
-        return redirect("/?veri_error=" + veri_error)
 
-    if len(email) > 1:
-        if '@' or '.' not in email:
-            if '' in email:
-                email_error = '''You need to have a proper email, mate.'''
-                return redirect("/?email_error=" + email_error)
-    else:
-        return render_template('welcome.html', user_name=name)
 
-@app.route('/')
+#make only one return of all errors
+#errors belong in HTML ready to go
+#render page of errors together at '/'
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    form_error1  = request.args.get('name_error', default='')
-    form_error2  = request.args.get('pass_error', default='')
-    form_error3  = request.args.get('veri_error', default='')
-    form_error4  = request.args.get('email_error', default='')
-    return render_template('user-signup.html', 
-    name_error=form_error1 and cgi.escape(form_error1, quote=True),
-    pass_error=form_error2 and cgi.escape(form_error2, quote=True),
-    veri_error=form_error3 and cgi.escape(form_error3, quote=True),
-    email_error=form_error4 and cgi.escape(form_error4, quote=True))
+
+    if request.method == 'POST':
+        password = request.form['password']
+        verify_password = request.form['verify_password']
+        name = request.form['user_name']
+        email = request.form['email']
+
+        password = cgi.escape(password)
+        verify_password = cgi.escape(verify_password)
+        name = cgi.escape(name)
+        email = cgi.escape(email)
+        is_error = 'False'
+
+        name_error = ''
+        pass_error = ''
+        veri_error = ''
+        email_error = ''
+
+        if len(name) < 3:
+            name_error = 'Name must be at least three characters.'
+            is_error = 'True'
+       
+
+        if len(password) < 3 or len(password) > 20:
+            pass_error = '''Password shouldn't be *that* short.'''
+            is_error = 'True'
+    
+
+        if password != verify_password:
+            veri_error = '''Passwords don't match'''
+            is_error = 'True'
+    
+
+        if len(email) >= 1:
+            if '@' and '.' not in email or ' ' in email:
+                email_error = '''You need to have a proper email, mate.'''
+                is_error = 'True'
+    
+
+
+        if is_error == 'True':    
+            return render_template('user-signup.html', name_error=name_error, pass_error=pass_error, 
+            veri_error=veri_error, email_error=email_error, user_name=name, email=email)
+        else:
+            return render_template('welcome.html', user_name=name)
+
+    return render_template('user-signup.html')
 app.run()
